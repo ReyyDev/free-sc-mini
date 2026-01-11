cc89a451cefe19a65deb6f0f59d88c3d702864e4
ReyyDev <117353741+ReyyDev@users.noreply.github.com>
2026-01-12 06:20:34 +0700

    config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ module.exports = {
     // Bot Settings
     PREFIX: process.env.BOT_PREFIX || '.',
     MODE: process.env.BOT_MODE || 'public', // 'self' or 'public'
-    BOT_NAME: process.env.BOT_NAME || 'ғʀᴇᴇ sᴄ ᴍɪɴɪ',
+    BOT_NAME: process.env.BOT_NAME || 'BotZ',
     VERSION: process.env.BOT_VERSION || '1.0.0',
-    BOT_FOOTER: process.env.BOT_FOOTER || 'ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴋɪɴɢ',
+    BOT_FOOTER: process.env.BOT_FOOTER || 'POWERED BY REYY',
     
     // Connection Settings
     MAX_RETRIES: parseInt(process.env.MAX_RETRIES) || 3,
@@ -18,7 +18,7 @@ module.exports = {
     AUTO_LIKE_EMOJI: ['💋', '🍬', '🫆', '💗', '🎈', '🎉', '🥳', '❤️', '🧫', '🐭'],
     
     // Group Settings
-    GROUP_INVITE_LINK: process.env.GROUP_INVITE_LINK || 'https://chat.whatsapp.com/H5jnuFUSOnGGcR5b5rCNdw',
+    GROUP_INVITE_LINK: process.env.GROUP_INVITE_LINK || 'https://chat.whatsapp.com/',
     
     // File Paths
     ADMIN_LIST_PATH: process.env.ADMIN_LIST_PATH || './admin.json',
@@ -34,19 +34,20 @@ module.exports = {
     NEWSLETTER_MESSAGE_ID: process.env.NEWSLETTER_MESSAGE_ID || '428',
     
     // Owner Settings
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '263714757857', // ᴀᴅᴅ ʏᴏᴜʀ ᴏᴡɴᴇʀ ɴᴜᴍʙᴇʀ
-    OWNER_NAME: process.env.OWNER_NAME || 'ᴍʀ xᴅ ᴋɪɴɢ', // ᴀᴅᴅ ʏᴏᴜʀ ᴏᴡɴᴇʀ ɴame
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '6285859738152', // ᴀᴅᴅ ʏᴏᴜʀ ᴏᴡɴᴇʀ ɴᴜᴍʙᴇʀ
+    OWNER_NAME: process.env.OWNER_NAME || 'Reyy', // ᴀᴅᴅ ʏᴏᴜʀ ᴏᴡɴᴇʀ ɴame
     
     // Channel Links
-    CHANNEL_LINK: process.env.CHANNEL_LINK || 'https://whatsapp.com/channel/0029VbB3YxTDJ6H15SKoBv3S',
+    CHANNEL_LINK: process.env.CHANNEL_LINK || 'https://whatsapp.com/channel/',
     
     // GitHub Settings
-    GITHUB_OWNER: process.env.GITHUB_OWNER || 'xdking0', //add your github username
+    GITHUB_OWNER: process.env.GITHUB_OWNER || 'ReyyDev', //add your github username
     GITHUB_REPO: process.env.GITHUB_REPO || 'session', // add your github repo name
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || '', //add your github token
     
     // API Keys
     TIKTOK_API_KEY: process.env.TIKTOK_API_KEY || 'free_key@maher_apis',
     NASA_API_KEY: process.env.NASA_API_KEY || '8vhAFhlLCDlRLzt5P1iLu2OOMkxtmScpO5VmZEjZ',
-    OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || '2d61a72574c11c4f36173b627f8cb177'
+    OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || '2d61a72574c11c4f36173b627f8
+        cb177'
 };
